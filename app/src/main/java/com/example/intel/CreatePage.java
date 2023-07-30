719c5be3fc69c367fed47f4216b52f1150d7d9c8
Utkarsh <utkarsh.more.2000@gmail.com>
2023-07-30 00:33:11 -0400

    firebase connected

--- app/src/main/java/com/example/intel/CreatePage.java
+++ app/src/main/java/com/example/intel/CreatePage.java
@@ -3,12 +3,22 @@ package com.example.intel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuthException;
 
 public class CreatePage extends AppCompatActivity {
+    private FirebaseAuthException auth;
 
-    @Override
+    private EditText signupEmail , signupPassword;
+    private Button signupButton;
+    private TextView loginReditectText;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_page);
+
     }
 }
