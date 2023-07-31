39c5b41e5421a246ca1a13b8c294f2005370d523
Utkarsh <utkarsh.more.2000@gmail.com>
2023-07-31 16:08:29 -0400

    firebase connected

--- app/src/main/java/com/example/intel/CreatePage.java
+++ app/src/main/java/com/example/intel/CreatePage.java
@@ -1,24 +1,74 @@
 package com.example.intel;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseUser;
 
 public class CreatePage extends AppCompatActivity {
-    private FirebaseAuthException auth;
-
-    private EditText signupEmail , signupPassword;
-    private Button signupButton;
-    private TextView loginReditectText;
+    EditText editTextEmail, editTextPassword, editTextFirstName, editTextLastName;
+    Button buttonReg;
+    FirebaseAuth mAuth;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_page);
+        mAuth = FirebaseAuth.getInstance();
+        editTextEmail = findViewById(R.id.email_input);
+        editTextPassword = findViewById(R.id.password_input);
+        editTextFirstName = findViewById(R.id.password_input);
+        editTextLastName = findViewById(R.id.password_input);
+        buttonReg = findViewById(R.id.create_btn);
+
+        buttonReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email , password;
+                email = String.valueOf(editTextEmail.getText());
+                password = String.valueOf(editTextPassword.getText());
+
+                if (TextUtils. isEmpty (email)){
+                    Toast.makeText(CreatePage.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils. isEmpty (password)){
+                    Toast.makeText(CreatePage.this, "Enter password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    // Sign in success, update UI with the signed-in user's information
+
+                                    FirebaseUser user = mAuth.getCurrentUser();
+
+                                } else {
+                                    // If sign in fails, display a message to the user.
+
+                                    Toast.makeText(CreatePage.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+
+                                }
+                            }
+                        });
+
+            }
+        });
 
     }
 }
